2eec6b8d398cde6ae43dee2bfc2402e252ee43e9
Jose Luis Plata <juch.youfather@gmail.com>
2025-10-23 12:56:09 -0600

    add cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
@@ -6,7 +7,7 @@ const { Resend} = require('resend');
 const port = process.env.PORT || 3009;
 
 app.use(express.json());
-
+app.use(cors());
 app.get("/", (req, res) => {
     res.send("Hello, World");
 });
