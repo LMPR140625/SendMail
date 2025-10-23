4a392b920672262b5d0d9515049e84c0ca4d073f
Jose Luis Plata <juch.youfather@gmail.com>
2025-10-23 14:42:36 -0600

    add some

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { Resend} = require('resend');
 const port = process.env.PORT || 3009;
 
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
 app.get("/", (req, res) => {
     res.send("Hello, World!!");
 });
